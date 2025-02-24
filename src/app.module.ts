5ff81c3c0aacab0f98cd908f4e78fc35bcc6457c
AKira0169 <odayterk75@gmail.com>
2025-02-24 21:00:33 +0200

    fix: remove duplicate import of ChatModule in app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ChatModule } from './chat/chat.module';
 import { UserModule } from './user/user.module';
 import { GroupsModule } from './groups/groups.module';
 import { ChatModule } from './chat/chat.module';
